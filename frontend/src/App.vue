70d20b9c330a32491b19599b4740bb66e05bec43
Clarence Baluyot <akingprincess@gmail.com>
2026-02-12 17:58:29 +0800

    feat: add Tailwinds and Daisy

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,21 +2,10 @@
 import RecipeList from './components/RecipeList.vue'
 </script>
 
-<template>
-  <RecipeList />
-</template>
+  <template>
+    <div class="max-w-6xl mx-auto p-4">
+      <RecipeList />
+    </div>
+  </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
